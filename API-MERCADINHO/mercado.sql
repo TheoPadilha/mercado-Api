3ec55db6c72a742c7639478d5908a45df31e5d33
TheoPadilha <168146283+TheoPadilha@users.noreply.github.com>
2025-02-27 19:52:33 -0300

    Update mercado.sql

--- API-MERCADINHO/mercado.sql
+++ API-MERCADINHO/mercado.sql
@@ -1,52 +1,112 @@
 -- Habilita a função gen_random_uuid() (se necessário)
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
--- Tabela de Produtos
-CREATE TABLE IF NOT EXISTS public.produtos (
-    id SERIAL PRIMARY KEY, -- ID numérico e autoincrementável
-    nome TEXT NOT NULL,
-    categoria TEXT NOT NULL, -- Categoria como texto
-    preco_in_cents INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL
-);
 
--- Tabela de Clientes
-CREATE TABLE IF NOT EXISTS public.clientes (
-    id SERIAL PRIMARY KEY, -- ID numérico e autoincrementável
-    nome TEXT NOT NULL,
-    cpf TEXT NOT NULL UNIQUE -- CPF único
-);
+-- Table: public.categorias
+CREATE TABLE IF NOT EXISTS public.categorias
+(
+    id integer NOT NULL DEFAULT nextval('categorias_id_seq'::regclass),
+    nome text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT categorias_pkey PRIMARY KEY (id),
+    CONSTRAINT categorias_nome_key UNIQUE (nome)
+)
 
--- Tabela de Compras
-CREATE TABLE IF NOT EXISTS public.compra (
-    id SERIAL PRIMARY KEY, -- ID numérico e autoincrementável
-    compra_date DATE NOT NULL,
-    usuario_id INTEGER NOT NULL,
-    CONSTRAINT fk_usuario
-        FOREIGN KEY (usuario_id)
-        REFERENCES clientes(id)
-);
+TABLESPACE pg_default;
 
--- Tabela de Produtos por Compra
-CREATE TABLE IF NOT EXISTS public.produtocompra (
-    id SERIAL PRIMARY KEY, -- ID numérico e autoincrementável
-    compra_id INTEGER NOT NULL,
-    produto_id INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL,
-    CONSTRAINT fk_compra
-        FOREIGN KEY (compra_id)
-        REFERENCES compra(id),
-    CONSTRAINT fk_produto
-        FOREIGN KEY (produto_id)
-        REFERENCES produtos(id)
-);
--- CREATE TABLE IF NOT EXISTS public.estoque (
---     id id SERIAL PRIMARY KEY,
---     produto_id UUID NOT NULL,
---     quantidade INTEGER NOT NULL,
---     data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     tipo_modificacao TEXT NOT NULL CHECK (tipo_modificacao IN ('entrada', 'saida')),
---     CONSTRAINT fk_produto
---         FOREIGN KEY (produto_id)
---         REFERENCES produtos(id)
--- );
+ALTER TABLE IF EXISTS public.categorias
+    OWNER to postgres;
+
+
+
+
+-- Table: public.clientes
+CREATE TABLE IF NOT EXISTS public.clientes
+(
+    id integer NOT NULL DEFAULT nextval('clientes_id_seq'::regclass),
+    nome text COLLATE pg_catalog."default" NOT NULL,
+    cpf text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT clientes_pkey PRIMARY KEY (id),
+    CONSTRAINT clientes_cpf_key UNIQUE (cpf)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.clientes
+    OWNER to postgres;
+
+
+
+
+-- Table: public.compra
+CREATE TABLE IF NOT EXISTS public.compra
+(
+    id integer NOT NULL DEFAULT nextval('compra_id_seq'::regclass),
+    compra_date timestamp without time zone NOT NULL,
+    cliente_id integer NOT NULL,
+    CONSTRAINT compra_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_usuario FOREIGN KEY (cliente_id)
+        REFERENCES public.clientes (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.compra
+    OWNER to postgres;
+
+
+
+
+
+-- Table: public.produtocompra
+CREATE TABLE IF NOT EXISTS public.produtocompra
+(
+    produto_id integer NOT NULL,
+    quantidade integer NOT NULL,
+    cliente_id integer NOT NULL,
+    compra_id integer NOT NULL DEFAULT nextval('produtocompra_compra_id_temp_seq'::regclass),
+    categoria_id integer,
+    preco numeric(10,2),
+    CONSTRAINT produtocompra_pkey PRIMARY KEY (produto_id, cliente_id, compra_id),
+    CONSTRAINT fk_categoria FOREIGN KEY (categoria_id)
+        REFERENCES public.categorias (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_cliente FOREIGN KEY (cliente_id)
+        REFERENCES public.clientes (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    CONSTRAINT fk_produto FOREIGN KEY (produto_id)
+        REFERENCES public.produtos (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.produtocompra
+    OWNER to postgres;
+
+
+
+-- Table: public.produtos
+CREATE TABLE IF NOT EXISTS public.produtos
+(
+    id integer NOT NULL DEFAULT nextval('produtos_id_seq'::regclass),
+    nome text COLLATE pg_catalog."default" NOT NULL,
+    quantidade integer NOT NULL,
+    categoria_id integer,
+    preco numeric(10,2) NOT NULL,
+    ativo boolean DEFAULT true,
+    CONSTRAINT produtos_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_categoria FOREIGN KEY (categoria_id)
+        REFERENCES public.categorias (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE SET NULL
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.produtos
+    OWNER to postgres;
