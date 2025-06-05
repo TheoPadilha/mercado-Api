cc6dd1ebe4627e50e3c738ca6317498f1422d655
TheoPadilha <168146283+TheoPadilha@users.noreply.github.com>
2025-06-05 10:33:34 -0300

    Update db.js

--- API-MERCADINHO/db.js
+++ API-MERCADINHO/db.js
@@ -13,6 +13,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Requerido pelo Neon
+  },
 });
 
 export default pool;
